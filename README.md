173c03b38095446b61785d6dc58679dd2ab31f08
brown <brown2243@naver.com>
2022-11-27 01:11:41 +0900

    hardhat: tutorial test

--- README.md
+++ README.md
@@ -6,13 +6,19 @@
 
 1. 이미지 빌드 `docker build -f Dockerfile.dev -t hello-solidity .`
 2. 컨테이너 실행 `docker run -v $(pwd)/app:/usr/app --rm -it hello-solidity /bin/bash`
+   1. 차후에 포트 연결 필요할 때 `-p`
 
 - OS: ubuntu:20.04
 - 패키지
 - build-essential vim wget curl jq nodejs yarn
 
-# hardhat-tutorial
+- jq는 뺼까 고민
+
+---
+
+# [hardhat-tutorial](https://hardhat.org/tutorial)
 
 - `yarn init`
 - `yarn add --dev hardhat`
 - `npx hardhat`
+- `npx hardhat compile`
--- app/hardhat-tutorial/test/Lock.ts
+++ app/hardhat-tutorial/test/Lock.ts
@@ -23,6 +23,34 @@ describe("Lock", function () {
     return { lock, unlockTime, lockedAmount, owner, otherAccount };
   }
 
+  describe("Token contract", function () {
+    it("Deployment should assign the total supply of tokens to the owner", async function () {
+      const [owner] = await ethers.getSigners();
+
+      const Token = await ethers.getContractFactory("Token");
+
+      const hardhatToken = await Token.deploy();
+
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+    it("Should transfer tokens between accounts", async function () {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+
+      const Token = await ethers.getContractFactory("Token");
+
+      const hardhatToken = await Token.deploy();
+
+      // Transfer 50 tokens from owner to addr1
+      await hardhatToken.transfer(addr1.address, 50);
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+
+      // Transfer 50 tokens from addr1 to addr2
+      await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+      expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+    });
+  });
+
   describe("Deployment", function () {
     it("Should set the right unlockTime", async function () {
       const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
